542b15f69612e9b35a884cb8b856e2305572d6f6
Pavel Kulikov <ppkulikov@gmail.com>
2022-07-14 18:48:12 +0300

    SOLVED!

--- src/main/java/com/epam/rd/autotasks/validations/ColorCodeValidation.java
+++ src/main/java/com/epam/rd/autotasks/validations/ColorCodeValidation.java
@@ -5,8 +5,17 @@ import java.util.regex.Pattern;
 
 public class ColorCodeValidation {
     public static boolean validateColorCode(String color) {
+        if (color != null){
+        Pattern colorCode = Pattern.compile("#[\\dA-Fa-f]{3}|#[\\dA-Fa-f]{6}");
+        return colorCode.matcher(color).matches();} else return false;
+
 
         // Put your code here
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println(validateColorCode("#abc"));
 
     }
 }
